8e411705905fa886eae9f93cf22e7c96675d8049
bluecrypt47 <lechuong09291999@gmail.com>
2020-12-22 13:59:57 +0700

    Sửa lại class thành className

--- photo-app/src/components/Signup/signup.jsx
+++ photo-app/src/components/Signup/signup.jsx
@@ -2,48 +2,48 @@ import "./style.css";
 
 function sign_up(){
     return(
-        <div class="signup">
-      <h1 class="signup_heading">Create account</h1>
+        <div className="signup">
+      <h1 className="signup_heading">Create account</h1>
 
-      <p class="signup_already">
+      <p className="signup_already">
         Already have an account?<a
           href="https://thethao99.com/wp-content/uploads/2020/05/gai-xinh-677.jpg"
-          class="signup_link signup-link-underline"
+          className="signup_link signup-link-underline"
         >
           Sign In</a
         >
       </p>
 
-      <form action="#" class="signup_form" autocomplete="off"></form>
+      <form action="#" className="signup_form" autocomplete="off"></form>
 
-      <div class="form_group">
+      <div className="form_group">
         <input
           type="text"
-          class="form_input"
+          className="form_input"
           placeholder="E-mail or phone..."
         />
       </div>
 
-      <div class="form_group form_group-2">
-        <input type="text" class="form_input" placeholder="Fisrt name" />
-        <input type="text" class="form_input" placeholder="Last name" />
+      <div className="form_group form_group-2">
+        <input type="text" className="form_input" placeholder="Fisrt name" />
+        <input type="text" className="form_input" placeholder="Last name" />
       </div>
 
-      <div class="form_group">
-        <input type="text" class="form_input" placeholder="Password" />
+      <div className="form_group">
+        <input type="text" className="form_input" placeholder="Password" />
       </div>
 
-      <div class="form_group">
-        <button type="submit" class="form_submit">Sign Up</button>
+      <div className="form_group">
+        <button type="submit" className="form_submit">Sign Up</button>
       </div>
 
-      <div class="form_tos">
+      <div className="form_tos">
         <input type="checkbox" name="" id="tos" />
         <label for="tos"
           >I have read and agree to the
           <a
             href="https://1.bp.blogspot.com/-vxzOoaZkuys/XJjzkeCxwWI/AAAAAAAAAJ4/mMaXATOC97sSebs1humLyyrLVGHmACb8QCLcBGAs/s1600/hinh-anh-gai-xinh-sexy-nong-bong.jpg"
-            class="signup_link"
+            className="signup_link"
             >Turn os Service</a
           >
         </label>
