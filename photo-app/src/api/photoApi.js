c5325b0fa216060d006889e5ec26aecf337daad5
malburo <tongquocbao0605@gmail.com>
2021-01-09 23:14:41 +0700

    update api for photoApi

--- photo-app/src/api/photoApi.js
+++ photo-app/src/api/photoApi.js
@@ -4,8 +4,20 @@ const photoApi = {
   getAll: () => {
     return request.get('photos');
   },
+  getAllOfMe: () => {
+    return request.get(`photos/users/me`);
+  },
+  getById: (photoId) => {
+    return request.get(`photos/${photoId}`);
+  },
   create: (payload) => {
     return request.post('photos', payload);
-  }
+  },
+  update: (payload) => {
+    return request.put(`photos/${payload.photoId}`, payload);
+  },
+  delete: (photoId) => {
+    return request.delete(`photos/${photoId}`);
+  },
 };
 export default photoApi;
